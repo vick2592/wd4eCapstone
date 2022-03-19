7a66275d48953e295a1db4b314394b6efee851ac
Victor <32042639+vick2592@users.noreply.github.com>
2022-03-19 01:55:54 -0400

    Add files via upload

---
+++ BootStrap Site/js/main.js
@@ -0,0 +1,45 @@
+// console.log(2 + 2);
+
+// $('.dropdown-submenu > a').on("click", function(e) {
+//     var submenu = $(this);
+//     $('.dropdown-submenu .dropdown-menu').removeClass('show');
+//     submenu.next('.dropdown-menu').addClass('show');
+//     e.stopPropagation();
+// });
+
+// $('.dropdown').on("hidden.bs.dropdown", function() {
+//     // hide any open menus when parent closes
+//     $('.dropdown-menu.show').removeClass('show');
+// });
+
+// Contact form Validator currently is offline due to not finished PHP file
+
+function validateForm() {
+  var name =  document.getElementById('name').value;
+  if (name == "") {
+      document.querySelector('.status').innerHTML = "Name cannot be empty";
+      return false;
+  }
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+          document.querySelector('.status').innerHTML = "Email format invalid";
+          return false;
+      }
+  }
+  var subject =  document.getElementById('subject').value;
+  if (subject == "") {
+      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+      return false;
+  }
+  var message =  document.getElementById('message').value;
+  if (message == "") {
+      document.querySelector('.status').innerHTML = "Message cannot be empty";
+      return false;
+  }
+  document.querySelector('.status').innerHTML = "Sending...";
+}
